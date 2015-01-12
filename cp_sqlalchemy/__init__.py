cc8b1b00dd1911b26fc4432d540cb10369130b7a
Eric Larson <eric@ionrock.org>
2015-01-11 22:12:43 -0600

    Bump version: 0.5.1 → 0.5.2

--- cp_sqlalchemy/__init__.py
+++ cp_sqlalchemy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Eric Larson'
 __email__ = 'eric@ionrock.org'
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 from .tool import SQLAlchemyTool  # noqa
 from .plugin import SQLAlchemyPlugin  # noqa
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='CherryPy-SQLAlchemy',
-    version='0.5.1',
+    version='0.5.2',
     description='Use SQLAlchemy with CherryPy',
     long_description=readme,
     author='Eric Larson',
