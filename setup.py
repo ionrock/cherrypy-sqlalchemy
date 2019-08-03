72b678464b533634718b6d88f05d565e92c36c73
Eric Larson <eric@ionrock.org>
2019-08-03 16:30:30 -0500

    Update the pypi link in the setup.py
    Fixes: #2
    Fixes: #6

--- setup.py
+++ setup.py
@@ -8,41 +8,35 @@ except ImportError:
     from distutils.core import setup
 
 
-readme = open('README.rst').read()
+readme = open("README.rst").read()
 
-requirements = [
-    'CherryPy',
-    'SQLAlchemy',
-]
+requirements = ["CherryPy", "SQLAlchemy"]
 
 setup(
-    name='CherryPy-SQLAlchemy',
-    version='0.5.2',
-    description='Use SQLAlchemy with CherryPy',
+    name="CherryPy-SQLAlchemy",
+    version="0.5.2",
+    description="Use SQLAlchemy with CherryPy",
     long_description=readme,
-    author='Eric Larson',
-    author_email='eric@ionrock.org',
-    url='https://github.com/ionrock/cp_sqlalchemy',
-    packages=[
-        'cp_sqlalchemy',
-    ],
-    package_dir={'cp_sqlalchemy':
-                 'cp_sqlalchemy'},
+    author="Eric Larson",
+    author_email="eric@ionrock.org",
+    url="https://github.com/ionrock/cherrypy-sqlalchemy",
+    packages=["cp_sqlalchemy"],
+    package_dir={"cp_sqlalchemy": "cp_sqlalchemy"},
     include_package_data=True,
     install_requires=requirements,
     license="BSD",
     zip_safe=False,
-    keywords='cp_sqlalchemy',
+    keywords="cp_sqlalchemy",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
 )
